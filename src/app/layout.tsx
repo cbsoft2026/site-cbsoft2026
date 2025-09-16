568f15a823dc3fa514d0dc3fb4f0be909f899d4b
guilhermeivo <guilhermeivob@gmail.com>
2025-09-15 23:26:58 -0300

    feat: custom title font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,15 @@ import getRequestConfig from '@/i18n/request';
 import ClientLayout from '@/providers/LocaleProvider';
 import { LayoutProvider } from '@/providers/LayoutProvider';
 
+import localFont from 'next/font/local';
+
+const gothamBlack = localFont({
+  src: '../../public/fonts/gotham-font/Gotham-Black.otf',
+  variable: '--font-gotham-black',
+  weight: '900',
+  style: 'normal',
+});
+
 type Props = {
   children: React.ReactNode;
 };
@@ -13,7 +22,7 @@ export default async function LocaleLayout({ children }: Props) {
   });
 
   return (
-    <html lang={locale}>
+    <html lang={locale} className={gothamBlack.variable}>
       <body>
         <ClientLayout locale={locale} messages={messages}>
           <LayoutProvider>{children}</LayoutProvider>
