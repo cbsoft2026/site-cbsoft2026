fa6dcf48c49b51b5d7add6ef32d4fb27f99d7e1e
guilhermeivo <guilhermeivob@gmail.com>
2025-10-10 16:07:53 -0300

    chore: pagina do simposio sblp

--- src/types/meta.ts
+++
@@ -1,8 +0,0 @@
-import { Type, Static } from '@sinclair/typebox';
-
-export const Meta = Type.Object({
-  slug: Type.String(),
-  name: Type.String(),
-});
-
-export type Meta = Static<typeof Meta>;
