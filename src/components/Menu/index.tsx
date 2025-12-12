eab426e4415da5d634957b25761acf340e475775
guilhermeivo <guilhermeivob@gmail.com>
2025-12-12 18:27:46 -0300

    fix: juntando trilhas no submenu do sbes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -106,9 +106,9 @@ export default function Menu(props: HTMLAttributes<HTMLDivElement>) {
       { title: t('sbes.educacao'), href: '/symposiums/sbes/educacao/call' },
       { title: t('sbes.ideias_inovadoras'), href: '/symposiums/sbes/ideias/call' },
       { title: t('sbes.ferramentas'), href: '/symposiums/sbes/tools/call' },
+      { title: t('sbes.industry'), href: '/symposiums/sbes/industry/call' },
       { title: t('sbes.ctic'), href: '/symposiums/sbes/ctic/call' },
       { title: t('sbes.ctd'), href: '/symposiums/sbes/ctd/call' },
-      { title: t('sbes.industry'), href: '/symposiums/sbes/industry/call' },
     ],
   };
 
