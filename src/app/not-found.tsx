ea77636b57735fce9dbc1c0576dec18eeb527dd7
Guilherme Ivo <guilhermeivob@gmail.com>
2026-06-08 12:31:54 -0300

    style: melhorando pagina de 404 not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,25 +4,9 @@ import Link from 'next/link';
 
 export default async function NotFound() {
   return (
-    <article
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        minHeight: '100svh',
-      }}
-    >
-      <h1
-        style={{
-          fontSize: '72px',
-          marginBottom: '72px',
-          textAlign: 'center',
-        }}
-      >
-        404 - Página não encontrada
-      </h1>
-      <div className={styles['background-geometric']} style={{ marginBottom: '72px' }}>
+    <article className={`container ${styles['not-found']}`}>
+      <h1>404 - Página não encontrada</h1>
+      <div className={styles['background-geometric']}>
         <BackgroundGeometric amount={9} />
       </div>
       <Link prefetch={false} href={'/'}>
