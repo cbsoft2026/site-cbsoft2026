6f9127cb5e2269c690186dc492fdf6fece3057eb
Thiago Colares <thicolares@gmail.com>
2026-06-18 14:20:07 -0400

    Drop old website structure. Use jekyll

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://cbsoft2026.ime.usp.br',
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-    changefreq: 'weekly'
-};
