537a3e481ef8a4d687f89ad3749dbeb3c06c853e
Guilherme Ivo <guilhermeivob@gmail.com>
2026-08-12 20:21:31 -0300

    chore: sblp: adicionado artigos (#145)

--- src/components/Menu/menuCollection.tsx
+++ src/components/Menu/menuCollection.tsx
@@ -128,6 +128,7 @@ export default function generatedCollection(t: Translator, commonT: Translator) 
     items: [
       { title: t('chamada_trabalhos'), href: '/symposiums/sblp/call' },
       { title: t('events'), href: '/symposiums/sblp/event' },
+      { title: t('artigos_aceitos'), href: '/symposiums/sblp/papers' },
     ],
   };
 
