d6e8815ddf922d42e8cb28794774134bb8db11cd
Guilherme Ivo <guilhermeivob@gmail.com>
2026-08-15 20:11:20 -0300

    refactor: ajuste para interface LaTeX

--- scripts/exportSessions.ts
+++ scripts/exportSessions.ts
@@ -115,8 +115,7 @@ function getEventsByDay(filter: (event: Event) => boolean) {
     content += `\\roomlocal{${local}}\n`;
     content += `\\dateinfo{${formatWeek(date)}}\n`;
 
-    content += '\\header\n';
-    content += '\\noindent\\begin{xltabular}[l]{0.8\\textwidth}{TS}\n';
+    content += '\\sessiontabular{%\n';
 
     Object.values(events)
       .sort((a, b) => {
@@ -126,9 +125,7 @@ function getEventsByDay(filter: (event: Event) => boolean) {
         content += renderSession(event);
       });
 
-    content += '\\end{xltabular}\n';
-    content += '\\newpage\n';
-
+    content += '}\n';
     return content;
   }
 
