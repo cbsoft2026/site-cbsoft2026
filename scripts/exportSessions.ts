aa3d74397ca855f6e54a1ff506613751d462d202
Guilherme Ivo <guilhermeivob@gmail.com>
2026-08-16 01:39:44 -0300

    feat: scripts: exportar sessoes por blocos de eventos (#135)

--- scripts/exportSessions.ts
+++ scripts/exportSessions.ts
@@ -80,7 +80,22 @@ function getEventsByDay(filter: (event: Event) => boolean) {
     namespace: 'common',
   });
 
-  function renderSession(event: Event) {
+  const rawEvents = mapToObject(loadEvents(locale));
+
+  function renderPapers(event: Event) {
+    const content: string[] = [];
+    if (event.parentIds && event.parentIds.length > 0) {
+      for (const parentId of event.parentIds) {
+        const parentEvent = rawEvents[parentId];
+        content.push(
+          `\\paper{${latexEscape(formatTime(parentEvent.schedule.start))}}{${latexEscape(parentEvent.title)}}{${latexEscape(parentEvent.participants.join(', '))}}`,
+        );
+      }
+    }
+    return content.join('\n');
+  }
+
+  function renderSession(event: Event, shouldRenderPapers: boolean) {
     const title = scheduleT.has(event.title) ? scheduleT(event.title) : event.title;
 
     const type = `${commonT(`eventos.${event.type}`)} · ${commonT(event.simposio)}`;
@@ -90,6 +105,8 @@ function getEventsByDay(filter: (event: Event) => boolean) {
           .map((moderator) => `\\speaker{${latexEscape(toSpeakerImage(moderator.image))}}`)
           .join('')}}`
       : '';
+
+    const papers = renderPapers(event);
 
     return [
       '\\sessiondivider',
@@ -101,10 +118,11 @@ function getEventsByDay(filter: (event: Event) => boolean) {
       moderators,
       '}',
       '\\\\[0.8cm]',
+      shouldRenderPapers ? papers : '',
     ].join('\n');
   }
 
-  function renderRoom(date: string, room: string, events: Record<string, Event>) {
+  function renderRoom(date: string, room: string, events: Record<string, Event>, shouldRenderPapers: boolean = false) {
     let content = '';
 
     const local = scheduleT.has(room) ? scheduleT(room) : room;
@@ -122,7 +140,7 @@ function getEventsByDay(filter: (event: Event) => boolean) {
         return new Date(a.schedule?.start ?? '').getTime() - new Date(b.schedule?.start ?? '').getTime();
       })
       .forEach((event) => {
-        content += renderSession(event);
+        content += renderSession(event, shouldRenderPapers);
       });
 
     content += '}\n';
@@ -141,6 +159,26 @@ function getEventsByDay(filter: (event: Event) => boolean) {
       }
       await generatePDF(paths.template, paths.output, `session-${date}`, content);
     }
+
+    const groupedByBlock = Object.entries(rawEvents)
+      .filter(([, event]) => {
+        return event.type != 'info' && event.type != 'artigo';
+      })
+      .reduce<Record<string, Record<string, Event>>>((acc, [id, event]) => {
+        if (!event.schedule) return acc;
+
+        acc[id] = event;
+
+        return acc;
+      }, {});
+
+    let content = '';
+    for (const [id, event] of Object.entries(groupedByBlock)) {
+      for (const room of event.rooms) {
+        content += renderRoom(event.schedule.start, room, { [`${id}`]: event }, true);
+      }
+    }
+    await generatePDF(paths.template, paths.output, `session-blocks`, content);
   }
 
   if (process.env.NODE_ENV === 'production') {
