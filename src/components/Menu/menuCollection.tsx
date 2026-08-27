34d012e4f1343cd6e3789fdcdae4d04cb2321c11
Guilherme Ivo <guilhermeivob@gmail.com>
2026-08-27 15:40:22 -0300

    chore: schedule: sast: adicionado programacao do sast (#188)

--- src/components/Menu/menuCollection.tsx
+++ src/components/Menu/menuCollection.tsx
@@ -200,6 +200,7 @@ export default function generatedCollection(t: Translator, commonT: Translator) 
     items: [
       { title: t('chamada_trabalhos'), href: '/symposiums/sast/call' },
       { title: t('events'), href: '/symposiums/sast/event' },
+      { title: t('artigos_aceitos'), href: '/symposiums/sast/papers' },
     ],
   };
 
