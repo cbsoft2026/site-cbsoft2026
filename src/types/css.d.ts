2b8b19ee8e27da2d460766210d13b9e9abe94a85
Guilherme Ivo <guilhermeivob@gmail.com>
2026-09-10 21:25:01 -0300

    fix: corrigido warning do editor de texto para pacotes css
    katex/dist/katex.min.css
    @fortawesome/fontawesome-svg-core/styles.css

--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -1,3 +1,7 @@
 declare module 'leaflet/dist/leaflet.css';
 
 declare module 'maplibre-gl/dist/maplibre-gl.css';
+
+declare module 'katex/dist/katex.min.css';
+
+declare module '@fortawesome/fontawesome-svg-core/styles.css';
